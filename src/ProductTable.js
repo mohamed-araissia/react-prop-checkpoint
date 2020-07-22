c44032fd4d0a89abdee73131d4c6dca76acb93c6
mohamed-araissia <63782409+mohamed-araissia@users.noreply.github.com>
2020-07-22 18:59:14 +0200

    Update ProductTable.js

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-const productarray=[{name:'t-shirt',price:'50',category:'clothes'},{name:'shoe',price:'250',category:'clothes'},{name:'smartphone',price:'850',category:'electronics'},{name:'laptop',price:'1500',category:'elctronics'}]
 
 export default function ProductTable(props) {
     return (
@@ -12,7 +11,7 @@ export default function ProductTable(props) {
     <th style={{color: "red"}}>Category</th>   
     </tr>  
     <tbody>
-    {productarray.map(prop => (
+    {props.productarray.map(prop => (
           <tr key={prop.name}>
             <td>{prop.name}</td>
             <td>{prop.price}</td>
