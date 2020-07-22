b7f5a6ccf4d821df7582106761420fbff9d1de06
mohamed-araissia <63782409+mohamed-araissia@users.noreply.github.com>
2020-07-22 18:59:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import ProductTable from './ProductTable'
 
+const productarray=[{name:'t-shirt',price:'50',category:'clothes'},{name:'shoe',price:'250',category:'clothes'},{name:'smartphone',price:'850',category:'electronics'},{name:'laptop',price:'1500',category:'elctronics'}]
+
 
 const productarray=[{name:'t-shirt',price:'50',category:'clothes'},{name:'shoe',price:'250',category:'clothes'},{name:'smartphone',price:'850',category:'electronics'},{name:'laptop',price:'1500',category:'elctronics'}]
 function App() {
